dc917d3f10daf639b2ef51ebd1d498cdb7a5e64b
leofaster <leofaster@PREDATOR>
2011-11-16 09:53:59 -0430

    Antes de salir a la univ

--- src/DumpXML.java
+++ src/DumpXML.java
@@ -1,12 +1,15 @@
-import net.n3.nanoxml.*;
-import java.io.*;
+import java.io.FileReader;
+import nanoxml.XMLElement;
+
+
+
 public class DumpXML
 {
   public static void main(String[] args)
       throws Exception
         {
 	    XMLElement xml = new XMLElement();
-            FileReader reader = new FileReader("AtariAmor.xspf");
+            FileReader reader = new FileReader("amor.xspf");
 	    xml.parseFromReader(reader);
             System.out.println(xml);
 	  }
